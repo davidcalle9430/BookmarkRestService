7d98f09335e36d984d018b5f9cdcea20ef068974
davidcalle9430 <davidcalle9430@gmail.com>
2015-12-13 01:30:17 -0500

    Edit account

--- src/main/java/web/UserController.java
+++ src/main/java/web/UserController.java
@@ -1,6 +1,7 @@
 package web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 import entities.Account;
@@ -35,6 +37,28 @@ public class UserController {
 		List<Account> users = accountRepository.findAll();
 		model.addAttribute("users" , users);
 		return "Usuarios";
+	}
+	@RequestMapping( value = "/EditarUsuario/", method = RequestMethod.GET)
+	public String editUser(@RequestParam String username, Model model){
+		Optional<Account> oAcount = accountRepository.findByUsername(username);
+		if(oAcount.isPresent()){
+			Account account = oAcount.get();
+			model.addAttribute("account", account);
+		}
+		return "EditarUsuario";
+	}
+	
+	
+	@RequestMapping(value="/EditarUsuario/", method=RequestMethod.POST)
+	public RedirectView postCreateUser( @ModelAttribute("account") Account account){
+		if(account.getPassword() == null || account.getPassword().length() < 1 ){
+			Account before = accountRepository.findOne(account.getId());
+			account.setPassword(before.getPassword());
+		}
+		System.out.println("Entra a editar al usuario ");
+		System.out.println(account.toString());
+		accountRepository.save(account);
+		return new RedirectView("/Usuarios/");
 	}
 	
 	
