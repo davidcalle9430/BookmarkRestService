985e6ec41e116e47fdbed8ad11a366ec439f5945
Fabian <merchanf@javeriana.edu.co>
2016-01-18 00:21:09 -0500

    trabajando en la opcion editar y guardar, falta guardar y probar

--- src/main/resources/static/js/inventario/actualizarPorcentajes.js
+++ src/main/resources/static/js/inventario/actualizarPorcentajes.js
@@ -1,6 +1,10 @@
 var alFinal = false;
 var pagina = 0;
-
+/*
+ * Realiza los llamados ajax, de ser exitoso el llamado
+ * llena la tabla, la idea es que como está separado por páginas, se
+ * solicite una página cada vez que se acabe la información mostrada.
+ * */
 function solicitarDatos(page){
 	$.ajax({
 		url: '/api/generos?page='+page,
@@ -14,6 +18,10 @@ function solicitarDatos(page){
 	});
 }
 
+/*
+ * Dado un genero, llena la tabla con la información obtenida desde
+ * la base de datos.
+ */
 function llenarTabla( generos ){
 	for(var i=0; i<generos.length; i++){
 		var codigo = $('<td>',{
@@ -22,18 +30,41 @@ function llenarTabla( generos ){
 		var nombre = $('<td>',{
 			text:generos[i].nombre 
 		})
-		var porcprec = $('<td>',{
-			text:generos[i].porcprec
+		var porcprec = $('<p>',{
+			text:generos[i].porcprec,
+			class: 'porcprec'
 		})
+		var tr_porcprec = $('<td>');
+		tr_porcprec.append(porcprec);
 		var fila = $('<tr>');
 		fila.append(codigo);
 		fila.append(nombre);
-		fila.append(porcprec);
+		fila.append(tr_porcprec);
 		$('tbody').append(fila);
 	}
 	
 }
 
+$('table').on('click', 'tr', function() {
+    var prev = $(this).find('.porcprec').text();
+    $(this).find('.porcprec').replaceWith("<div class=\"row\">"
+    +"<div class=\"large-4 large-offset-4 columns\">"
+    +"<div class=\"row collapse\">"
+    +"  <div class=\"small-10 columns\">"
+    +"     <input type=\"text\" value=\""+prev+"\">"
+    +"  </div>"
+    +"  <div class=\"small-2 columns\">"
+    +"     <a href=\"#\" class=\"button postfix guardar-btn\">Guardar</a>"
+    +"  </div>"
+    +"</div>"
+  +"</div>");
+});
+
+/*
+ * Acciones a ejecutar cuando se terminen de cargar los elementos en la
+ * pantalla (html, css). En este caso se ejecuta un codigo que permite
+ * carga la información por partes según se vayan viendo y solicitando. 
+ * */
 $(document).ready(function(){
 	solicitarDatos(pagina);
 	$(window).scroll(function() {
