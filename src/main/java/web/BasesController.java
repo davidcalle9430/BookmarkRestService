c33e695f610d7ea17b8adce8be4c0148b4226ab2
David Calle <davidcalle9430@gmail.com>
2016-02-27 21:06:23 -0500

    Crear Base

--- src/main/java/web/BasesController.java
+++ src/main/java/web/BasesController.java
@@ -5,8 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BasesController {
+	
 	@RequestMapping(value="/bases/")
 	public String bases(){
 		return "bases/bases";
+	}
+	@RequestMapping(value="/bases/editar/")
+	public String basesEditar(){
+		return "bases/baseseditar";
+	}
+	@RequestMapping(value="/bases/nuevo/")
+	public String basesNuevo(){
+		return "bases/basesnuevo";
 	}
 }
