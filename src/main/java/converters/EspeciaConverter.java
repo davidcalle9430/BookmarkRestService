78fd4bc8269ea1ac11411d7aa7cf0f12a292df82
David Calle <davidcalle9430@gmail.com>
2016-06-06 20:31:59 -0500

    Caso de uso terminado

--- src/main/java/converters/EspeciaConverter.java
+++ src/main/java/converters/EspeciaConverter.java
@@ -1,7 +1,6 @@
 package converters;
 
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.util.SocketUtils;
 
 import sidic.entities.EspeciaPK;
 
@@ -11,10 +10,8 @@ public class EspeciaConverter implements Converter<String, EspeciaPK> {
 	public EspeciaPK convert(String uri) {
 		
 		String[] arreglo = uri.split("--");
-		System.out.println("El string que lelga es  " + uri + " dividido por  " + arreglo[ 0 ]+ " y " + arreglo[ 1 ]);
 		long one = (long)Integer.parseInt(arreglo[0]);
 		long two = (long)Integer.parseInt(arreglo[1]);
-		System.out.println("los num son " + one +" y "+ two);
 		EspeciaPK pk = new EspeciaPK( one , two );
 		return pk;
 	}
